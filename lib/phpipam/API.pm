a5a2b82983a77d385ad02bff6207c23ca410b922
Natxo Asenjo <natxo@asenjo.nl>
2016-07-26 14:17:17 +0200

    new methods search_ip, get_addrs_subnet, get_ip_tags, get_ips_tag improved error responses, perltidy

--- lib/phpipam/API.pm
+++ lib/phpipam/API.pm
@@ -2,7 +2,6 @@ package phpipam::API;
 
 use strict;
 use warnings;
-
 use Mojo::UserAgent;
 
 # web browser
@@ -17,9 +16,9 @@ my ( $url, $prot, $api, $user, $pass );
 # constructor
 sub new {
     my ( $class, %args ) = @_;
-    $url = $args{url};
+    $url  = $args{url};
     $prot = $args{proto};
-    $api = $args{api};
+    $api  = $args{api};
     $pass = $args{password};
     $user = $args{user};
     my $self = {
@@ -155,6 +154,17 @@ sub free_first_address {
 
 }    ## --- end sub free_first_address
 
+
+#===  FUNCTION  ================================================================
+#         NAME: get_rights
+#      PURPOSE: get rights of logged in api user
+#   PARAMETERS: token
+#      RETURNS: hash reference with 2 keys: controllers and permissions
+#  DESCRIPTION: see name
+#       THROWS: no exceptions
+#     COMMENTS: none
+#     SEE ALSO: n/a
+#===============================================================================
 sub get_rights {
     my ( $self, $token ) = @_;
 
@@ -167,11 +177,23 @@ sub get_rights {
     }
     else {
         my $err = $tx->error;
-        die "Cannot get rights $err->{code} response -> $err->{message}";
+        warn "cannot get rights, error: $err->{code} "
+          . $tx->res->json->{message}, "\n";
 
     }
 }
 
+
+#===  FUNCTION  ================================================================
+#         NAME: search_subnet
+#      PURPOSE: get subnet info 
+#   PARAMETERS: token, subnet, mask
+#      RETURNS: array ref with hash describing subnet
+#  DESCRIPTION: see name
+#       THROWS: no exceptions
+#     COMMENTS: none
+#     SEE ALSO: n/a
+#===============================================================================
 sub search_subnet {
     my ( $self, $token, $subnet, $mask ) = @_;
     die "Sorry, no token found\n" unless defined $token;
@@ -189,12 +211,24 @@ sub search_subnet {
     }
     else {
         my $err = $tx->error;
-        die
-"Cannot find subnet $subnet/$mask: $err->{code} response -> $err->{message}";
+        die warn "cannot find subnet $subnet/$mask, error: $err->{code} "
+          . $tx->res->json->{message}, "\n";
 
     }
 }
 
+
+#===  FUNCTION  ================================================================
+#         NAME: add_ip
+#      PURPOSE: add ip address to ipam
+#   PARAMETERS: token, subnetid, ip, hostname, mac
+#      RETURNS: if successfully added, location header on stdout with ip
+#      info
+#  DESCRIPTION: see name
+#       THROWS: no exceptions
+#     COMMENTS: 
+#     SEE ALSO: n/a
+#===============================================================================
 sub add_ip {
     my ( $self, %args ) = @_;
     my $token    = $args{token};
@@ -211,19 +245,34 @@ sub add_ip {
         "$prot://$url$api/addresses/" => { 'token' => $token } => json => {
             'ip'       => $ip,
             'subnetId' => $subnetid,
+            'hostname' => $hostname,
+            'mac'      => $macaddr,
         }
     );
+
     if ( $tx->success ) {
         print "$ip successfully added\n";
         print $tx->res->content->headers->location, "\n";
     }
     else {
         my $err = $tx->error;
-        die "cannot add $ip $err->{code} response -> $err->{message}";
+        warn "cannot add $ip, error: $err->{code} " . $tx->res->json->{message},
+          "\n";
     }
 
 }
 
+
+#===  FUNCTION  ================================================================
+#         NAME: delete_ip
+#      PURPOSE: remove ip from ipa 
+#   PARAMETERS: token, ip_id
+#      RETURNS: 
+#  DESCRIPTION: 
+#       THROWS: no exceptions
+#     COMMENTS: none
+#     SEE ALSO: n/a
+#===============================================================================
 sub delete_ip {
     my ( $self, %args ) = @_;
     my $token = $args{token};
@@ -233,15 +282,15 @@ sub delete_ip {
       if !defined $token or !defined $ip_id;
 
     my $tx = $ua->delete(
-        "$prot://$url$api/addresses/$ip_id/" => { 'token' => $token }
-    );
+        "$prot://$url$api/addresses/$ip_id/" => { 'token' => $token } );
 
     if ( $tx->success ) {
         print "$ip_id successfully deleted\n";
     }
     else {
         my $err = $tx->error;
-        die "cannot delete $ip_id $err->{code} response -> $err->{message}";
+        warn "cannot delete $ip_id, error: $err->{code} "
+          . $tx->res->json->{message}, "\n";
     }
 }
 
@@ -259,22 +308,148 @@ sub delete_ip {
 sub search_hostname {
     my ( $self, %args ) = @_;
     my $token = $args{token};
-    my $host = $args{host};
+    my $host  = $args{host};
 
     die "I need both the hostname as the token\n"
       if !defined $token or !defined $host;
 
     my $tx = $ua->get(
-        "$prot://$url$api/addresses/search_hostname/$host/" => { 'token' => $token }
-    );
+        "$prot://$url$api/addresses/search_hostname/$host/" => { 'token' => $token } );
 
     if ( $tx->success ) {
         return $tx->res->json('/data');
     }
     else {
         my $err = $tx->error;
-        die "cannot find host $host $err->{code} response -> $err->{message}";
+        warn "cannot find host $host, error: $err->{code} "
+          . $tx->res->json->{message}, "\n";
     }
 }
+
+
+#===  FUNCTION  ================================================================
+#         NAME: search_ip
+#      PURPOSE: find ip details in ipam
+#   PARAMETERS: token, ip
+#      RETURNS: array of hashes ref
+#  DESCRIPTION: 
+#       THROWS: no exceptions
+#     COMMENTS: none
+#     SEE ALSO: n/a
+#===============================================================================
+sub search_ip {
+    my ( $self, %args ) = @_;
+    my $token = $args{token};
+    my $ip    = $args{ip};
+
+    die "I need both the token as the ip\n"
+      if !defined $token or !defined $ip;
+
+    my $tx = $ua->get( "$prot://$url$api/addresses/search/$ip/" =>
+          { 'token' => $token } );
+
+    if ( $tx->success ) {
+        return $tx->res->json('/data');
+    }
+    else {
+        my $err = $tx->error;
+        die "cannot search $ip, error: $err->{code} "
+          . $tx->res->json->{message}, "\n";
+    }
+}    ## --- end sub search_ip
+
+
+#===  FUNCTION  ================================================================
+#         NAME: get_addrs_subnet
+#      PURPOSE: get info addresses in subnet
+#   PARAMETERS: token, subnetid
+#      RETURNS: array of hashes 
+#  DESCRIPTION: 
+#       THROWS: 
+#     COMMENTS: none
+#     SEE ALSO: n/a
+#===============================================================================
+sub get_addrs_subnet {
+    my ( $self, %args ) = @_;
+    my $token    = $args{token};
+    my $subnetid = $args{subnetid};
+
+    die "I need both the subnet id as the token\n"
+      if !defined $token or !defined $subnetid;
+
+    my $tx = $ua->get( "$prot://$url$api/subnets/$subnetid/addresses/" =>
+          { 'token' => $token } );
+
+    if ( $tx->success ) {
+        return $tx->res->json('/data');
+    }
+    else {
+        my $err = $tx->error;
+        die
+"cannot find addresses in subnet $subnetid $err->{code} response -> $err->{message}";
+    }
+
+}
+
+#===  FUNCTION  ================================================================
+#         NAME: get_ip_tags
+#      PURPOSE: retrieve all ip tags from phpipam
+#   PARAMETERS: $token
+#      RETURNS: all ip tags
+#  DESCRIPTION:
+#       THROWS: no exceptions
+#     COMMENTS: not working yet, see https://github.com/phpipam/phpipam/issues/632
+#     SEE ALSO: n/a
+#===============================================================================
+sub get_ip_tags {
+    my ( $self, %args ) = @_;
+    my $token = $args{token};
+
+    die "I need both the the token\n"
+      if !defined $token;
+
+    my $tx =
+      $ua->get( "$prot://$url$api/addresses/tags/" => { 'token' => $token } );
+
+    if ( $tx->success ) {
+        return $tx->res->json('/data');
+    }
+    else {
+        my $err = $tx->error;
+        die "cannot find ip tags $err->{code} response -> $err->{message}";
+    }
+
+}
+
+#===  FUNCTION  ================================================================
+#         NAME: get_ips_tag
+#      PURPOSE: retrieve ips assigned to tag id
+#   PARAMETERS: token, tagid
+#      RETURNS: array ref with hashes
+#  DESCRIPTION:
+#       THROWS: http errors
+#     COMMENTS: none
+#     SEE ALSO: n/a
+#===============================================================================
+sub get_ips_tag {
+    my ( $self, %args ) = @_;
+    my $token = $args{token};
+    my $tagid = $args{tagid};
+
+    die "I need both the the token and the tag id\n"
+      if !defined $token or !defined $tagid;
+
+    my $tx = $ua->get( "$prot://$url$api/addresses/tags/$tagid/addresses/" =>
+          { 'token' => $token } );
+
+    if ( $tx->success ) {
+        return $tx->res->json('/data');
+    }
+    else {
+        my $err = $tx->error;
+        die "cannot find ip tags $err->{code} response -> $err->{message}";
+    }
+
+}    ## --- end sub get_ips_tag
 
 1;
