8cabcd505519ce7c6de7a5255e32a5a51727f538
Natxo Asenjo <natxo@asenjo.nl>
2018-06-17 11:48:42 +0200

    added delete_token method

--- lib/phpipam/Token/Token.pm
+++ lib/phpipam/Token/Token.pm
@@ -28,4 +28,27 @@ sub get_token {
     }
 }
 
+=head3 B<delete_token>
+Remove the api token.
+    $ipam->delete_token( $token );
+If assigned to a scalar variable, it returns a json string with info.
+The method requires the $token parameter.
+=cut
+
+sub delete_token {
+    my ( $self, $token ) = @_;
+    die "Need token to delete_token\n" unless defined $token;
+
+    my $tx = $ua->delete( "$prot://$url$api/user/" => { 'token' => $token } );
+
+    if ( $tx->success ) {
+        return $tx->res->json('/data')->[0];
+    }
+    else {
+        my $err = $tx->error;
+        die "Could not delete token!  $err->{code} "
+          . $tx->res->json('/message');
+    }
+}    ## --- end sub delete_token
+
 1;
